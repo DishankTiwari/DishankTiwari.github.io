608562e66ca351c1a17c92cb74adf1d4bb9628fe
DishankTiwari <dishank@fusioncharts.com>
2015-07-16 18:53:27 +0530

    HTML_CSS_DOM

--- Learning/Introduction To Html/basic_DOM.js
+++ Learning/Introduction To Html/basic_DOM.js
@@ -1,22 +1,56 @@
-function tab(i){
-if(i===1){
-    document.getElementById("News").style.visibility="visible";
-    document.getElementById("subs").style.visibility="hidden";
-    document.getElementById("tab2").style.background="white";
-    document.getElementById("tab1").style.background="#e4efff";
-    document.getElementById("tab2_a").style.color=" #6e6e6e  ";
-    document.getElementById("tab1_a").style.color="#1b6bce";
-    document.getElementById("tab1").style.zIndex=1;
-    document.getElementById("tab2").style.zIndex=0;
-  }
-  else{
-    document.getElementById("News").style.visibility="hidden";
-    document.getElementById("subs").style.visibility="visible";
-    document.getElementById("tab2").style.background="#e4efff";
-    document.getElementById("tab1").style.background="white";
-    document.getElementById("tab1_a").style.color=" #6e6e6e  ";
-    document.getElementById("tab2_a").style.color="#1b6bce";
-    document.getElementById("tab1").style.zIndex=0;
-    document.getElementById("tab2").style.zIndex=1;
-  }
+function tab1() {
+  document.getElementById("news").style.color="blue";
+  document.getElementById("sub").style.color="black";
+  document.getElementById("news").style.background="#E5EFFF";
+  document.getElementById("sub").style.background="none";
+  document.getElementById("left").style.display="block";
+  document.getElementById("right").style.display="block";
+  document.getElementById("lern").style.display="block";
+  document.getElementById("form").style.display="none";
+  document.getElementById("subscribe").style.display="none";
+}
+function tab2()
+{
+  document.getElementById("news").style.color="black";
+  document.getElementById("sub").style.color="blue";
+  document.getElementById("sub").style.background="#E5EFFF";
+  document.getElementById("news").style.background="none";
+  document.getElementById("left").style.display="none";
+  document.getElementById("right").style.display="none";
+  document.getElementById("lern").style.display="none";
+  document.getElementById("form").style.display="block";
+  document.getElementById("subscribe").style.display="block";
+}
+function jsonval()
+{
+
+  var interest="";
+  var name=document.form1name.name.value;
+  var email=document.form1name.email.value;
+  var sex=document.form1name.sex.value;
+  var inputElements = document.getElementsByClassName('inter');
+  for(var i=0; inputElements[i]; ++i)
+  {
+     	  if(inputElements[i].checked)
+        {
+ 
+          interest= interest+inputElements[i].value+" ";
+          //console.log(interest);	
+
+        }
+  }
+  var country=document.form1name.country.value;
+  var address=document.form1name.address.value;
+  var object=[];
+  var array=[];
+  object.name=name;
+  object.country=country;
+  object.email=email;
+  object.address=address;
+  object.sex=sex;
+  object.arr=interest;
+  JSON.stringify(object);
+  
+console.log(object);
+
 }
