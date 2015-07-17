bd2cd9ab0104e6af654c8601c994b25d6fa4380f
DishankTiwari <dishank@fusioncharts.com>
2015-07-17 11:25:09 +0530

    Edited

--- Learning/Introduction To Html/basic_DOM.js
+++ Learning/Introduction To Html/basic_DOM.js
@@ -20,7 +20,7 @@ function tab2() {
   document.getElementById("form").style.display="block";
   document.getElementById("subscribe").style.display="block";
 }
-function jsonval() {
+function jsonVal() {
   var interest="";
   var name=document.form1name.name.value;
   var email=document.form1name.email.value;
@@ -32,15 +32,37 @@ function jsonval() {
     }
   }
   var country=document.form1name.country.value;
-  var address=document.form1name.address.value;
-  var object=[];
+  var description=document.form1name.description.value;
+  var object={};
   var array=[];
   object.name=name;
   object.country=country;
   object.email=email;
-  object.address=address;
+  object.description=description;
   object.sex=sex;
   object.interest=interest;
   JSON.stringify(object);
   console.log(object);
+}
+function optns(){
+  if(document.getElementById("country").options[0].selected){
+    document.getElementById("country").options[0].disabled=true;
+    document.getElementById("country").options[1].disabled=false;
+    document.getElementById("state1").style.visibility="visible";
+    document.getElementById("state2").style.visibility="hidden";
+  }
+  else{
+    document.getElementById("country").options[0].disabled=false;
+    document.getElementById("country").options[1].disabled=true;
+    document.getElementById("state1").style.visibility="hidden";
+    document.getElementById("state2").style.visibility="visible";
+  }
+  document.getElementById("state1_name").style.visibility="visible";
+}
+function movieType(){
+  l = document.getElementById("movieType");
+  l.style.visibility = (l.style.visibility == "visible") ? "hidden" : "visible";
+}
+function back(){
+  document.getElementById("movieType").style.visibility == "hidden";
 }
