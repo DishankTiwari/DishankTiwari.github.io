6af6f09d093402016fc684e44e28f18e15e787e6
DishankTiwari <dishank@fusioncharts.com>
2015-07-30 14:03:15 +0530

    PHP data

---
+++ Learning/Introduction_to_php/data.php
@@ -0,0 +1,46 @@
+<!DOCTYPE Html>
+<html>
+<head>
+	<title>Database</title>
+<?php
+	$servername = "127.0.0.1";
+	$user_name = "root";
+	$password = "";
+	$database = "dishank";
+
+	$db_handle = mysql_connect($servername, $user_name, $password);
+	$db_found = mysql_select_db($database, $db_handle);
+
+	if ($db_found) {
+		$SQL = "SELECT * FROM subscribe";
+		$result = mysql_query($SQL);
+
+		while ( $db_field = mysql_fetch_assoc($result) ) {
+			?>
+			<table width="650" border="1" cellspacing="0" cellpadding="5">
+				<tr>
+					<td><? print $db_field['name']; ?></td>
+					<td><? print $db_field['name']; ?></td>
+					<td><? print $db_field['email']; ?></td>
+					<td><? print $db_field['number']; ?></td>
+					<td><? print $db_field['country']; ?></td>
+					<td><? print $db_field['sex']; ?></td>
+					<td><? print $db_field['description'] . "<BR>"; ?></td>
+					<td><input type="submit" name="submit" value="Edit"><?  ?></td>
+					<td><input type="submit" name="submit" value="Delete"><? mysql_query("DELETE FROM subscribe WHERE $db_field[$email] ='email'");  ?></td>
+				</tr>
+			</table>
+					<?php
+		}
+		mysql_close($db_handle);
+	}
+	else {
+		print "Database NOT Found";
+		mysql_close($db_handle);
+	}
+?>
+</head>
+<body>
+	
+</body>
+</html>
--- Learning/Introduction_to_php/index.php
+++ Learning/Introduction_to_php/index.php
@@ -6,7 +6,8 @@
   <script src="basic_DOM.js"></script>
   <?php
   $name_error = $email_error = $country_error = $description_error = $sex_error = $interest_error = $number_error = "";
-  $name = $email = $country = $description = $sex = $interest = $number = $message = "";
+  $name = $email = $country = $description = $sex = $number = $message = "";
+  $interest ="";
   $Flag=0;
   if ($_SERVER["REQUEST_METHOD"] == "POST") { $Flag=1;
     if (empty($_POST["name"])) {
@@ -14,14 +15,14 @@
       $Flag = 0;
     }
     else {
-      $name = value($_POST["name"]);
+      $name = $_POST["name"];
     }
     if (empty($_POST["email"])) {
       $email_error = "Email is required";
       $Flag = 0;
     }
     else {
-      $email = value($_POST["email"]);
+      $email = $_POST["email"];
       if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $email_error = "Invalid Email";
       }
@@ -31,53 +32,76 @@
       $Flag = 0;
     }
     else {
-      $country = value($_POST["country"]);
+      $country = $_POST["country"];
     }
     if (empty($_POST["description"])) {
       $description_error = "Required";
       $Flag = 0;
     }
     else {
-      $description = value($_POST["description"]);
+      $description = $_POST["description"];
     }
     if (empty($_POST["sex"])) {
       $sex_error = "Sex is required";
       $Flag = 0; 
     }
     else {
-      $sex = value($_POST["sex"]);
+      $sex = $_POST["sex"];
     }
     if (empty($_POST["interest"])) {
       $interest_error = "Required";
       $Flag = 0;
     }
     else {
-      $interest = value($_POST["interest"]);
+      $interest = $_POST["interest"];
     }
     if (empty($_POST["number"])) {
       $number_error = "Number is required";
       $Flag = 0;
     }
     else {
-      $number = value($_POST["number"]);
+      $number = $_POST["number"];
       if(!preg_match("/^[789][0-9]{9}$/", $number)){
         $number_error = "Invalid Number";
       }
     }
-  }
-  function value($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-  }
     if($Flag == 1){
-    $data = "$name, $email, $number, $country, $sex, $interest,$description \n";
-    $myf = fopen("/home/dishanktiwari/DishankTiwari.github.io/Learning/Introduction_to_php/details.csv", "a");
-    fwrite($myf, $data);
-    fclose($myf);
-    $message = "Registered Successfully";  
+    //<-- $data = "$name, $email, $number, $country, $sex, $interest,$description \n";
+    //$myf = fopen("/home/dishanktiwari/DishankTiwari.github.io/Learning/Introduction_to_php/details.csv", "a");
+    //fwrite($myf, $data);
+    //fclose($myf); -->
+      $message = "Registered Successfully";  
+
+      $servername = "127.0.0.1";
+      $username = "root";
+      $password = "";
+      $dbname = "dishank";
+
+      $conn = mysqli_connect($servername, $username, $password, $dbname);
+      if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+      }
+
+      $sql = "INSERT INTO subscribe (name, email, number, country, sex, description) VALUES ('$name', '$email', '$number', '$country', '$sex', '$description')";
+      if (mysqli_query($conn, $sql)) {
+        echo "New record created successfully";
+      } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+      }
+      $sql1 = "INSERT INTO interest (email, hobby1, hobby2, hobby3) VALUES ('$email', '$interest[0]', '$interest[1]', '$interest[2]')";
+     
+      mysqli_query($conn, $sql1);
+
+    mysqli_close($conn);
     }
+  }
+  // function value($data) {
+  //   $data = trim($data);
+  //   $data = stripslashes($data);
+  //   $data = htmlspecialchars($data);
+  //   return $data;
+  // }
+  
   ?>
 </head>
 <body>
@@ -127,9 +151,9 @@
             </tr>
             <tr>
               <td>Interest</td>
-              <td><input type="checkbox" name="interest" <? if (isset($interest) && $interest=="Football") echo "checked"; ?> value="Football">Football
-                <input type="checkbox" name="Interest" <? if (isset($interest) && $interest=="Movie") echo "checked"; ?> value="Movie" onclick="movieType();">Movie
-                <input type="checkbox" name="Interest" <? if (isset($interest) && $interest=="Reading") echo "checked"; ?> value="Reading" >Reading <span>* <?php echo $interest_error;?></span></td>
+              <td><input type="checkbox" name="interest[]" <?php if (is_array($interest)){ foreach($interest as $value){ if($value == 'Football' ) echo "checked"; }} ?> value="Football">Football
+                <input type="checkbox" name="interest[]"<?php if (is_array($interest)){ foreach($interest as $value){ if($value == 'Movie' ) echo "checked"; }} ?> value="Movie" onclick="movieType();">Movie
+                <input type="checkbox" name="interest[]" <?php if (is_array($interest)){ foreach($interest as $value){ if($value == 'Reading' ) echo "checked"; }} ?> value="Reading" >Reading <span>* <?php echo $interest_error;?></span></td>
             </tr>
           </table>
           <button type="reset" value="Reset" class="buttons resetButtons">RESET</button>
